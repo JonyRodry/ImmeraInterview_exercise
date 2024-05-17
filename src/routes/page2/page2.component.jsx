0ec91df34c499e426708c8e9433a0dd94526417e
JonyRodry <joao_manuel1999@hotmail.com>
2024-05-17 22:40:34 +0100

    Add API

--- src/routes/page2/page2.component.jsx
+++ src/routes/page2/page2.component.jsx
@@ -1,9 +1,13 @@
+import API from "../../components/api/api.componente";
 import Slider from "../../components/slider/slider.component";
 
 function Page2() {
   
   return (
-    <Slider />
+    <>
+      <Slider />
+      <API />
+    </>
   );
 }
 
