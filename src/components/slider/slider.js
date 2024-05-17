25f120598231ce5f3246380d0915c920bd82ceea
JonyRodry <joao_manuel1999@hotmail.com>
2024-05-17 12:42:42 +0100

    Add slider

---
+++ src/components/slider/slider.js
@@ -0,0 +1,18 @@
+const slides = [
+    {
+        id: 1, 
+        name: 'Slide 11111', 
+        description: 'Primeiro slide' 
+    },
+    {
+        id: 2, 
+        name: 'Slide 22222', 
+        description: 'Segundo slide.' 
+    },
+    {id: 3, 
+        name: 'Slide 33333', 
+        description: 'Terceiro slide.' 
+    },
+];
+
+export default slides;
--- src/routes/page1/page1.component.jsx
+++ src/routes/page1/page1.component.jsx
@@ -1,9 +1,11 @@
 import BasicContent from "../../components/basicContent/basicContent.component";
+import Slider from "../../components/slider/slider.component";
 
 function Page1() {
   
   return (
     <>
+      <Slider></Slider>
       <BasicContent></BasicContent>
     </>
   );
